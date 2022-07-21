4ca514a59abe69d16dddeaae9b1a1595d6afc8a2
Legend28469 <johnathaneb@gmail.com>
2022-07-21 12:18:13 -0400

    Added check to backend for empty image string

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -150,6 +150,10 @@ router.post("/", auth.required, function (req, res, next) {
 
       item.seller = user;
 
+      if (item.image === "") {
+        item.image = "/placeholder.png";
+      }
+
       return item.save().then(function () {
         sendEvent("item_created", { item: req.body.item });
         return res.json({ item: item.toJSONFor(user) });
